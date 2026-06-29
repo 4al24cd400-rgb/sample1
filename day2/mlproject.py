9b2bc251cfec99718b889b87f99a0770abe73a11
Student <student@example.com>
2026-06-29 12:33:58 +0530

    first commit

---
+++ day2/mlproject.py
@@ -0,0 +1,102 @@
+from IPython.core import getipython
+from IPython.core import getipython
+from re import X
+import selectors
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import os
+from sklearn.feature_selection import SelectKBest, mutual_info_regression
+
+# category_encoders is needed for Target Encoding text
+
+try:
+    # pyrefly: ignore [missing-import]
+    from category_encoders import TargetEncoder
+except ImportError:
+    TargetEncoder = None
+    print("Warning: category_encoders not installed. Target Encoding will not be available.")
+
+
+def main():
+
+    print("Loading Dataset")
+    file_path = "train.csv"
+
+    if not os.path.exists(file_path):
+        print(f"Error: Cannot find '{file_path}'")
+        return
+
+    df = pd.read_csv(file_path)
+    print(f"Dataset Loaded Successfully, Rows: {df.shape[0]}, Features: {df.shape[1]}\n")
+
+    df["Team_ID"] = ["Team_" + str(np.random.randint(1,150)) for _ in range(len(df))]
+
+    if TargetEncoder is not None:
+        print("Applying Target Encoder")
+
+        encoder = TargetEncoder()
+
+        df["Team_ID_Encoder"] = encoder.fit_transform(df["Team_ID"], df["H"])
+
+    else:
+        print("Category Encoder not installed")
+
+    # Feature Selection
+
+    features_to_test = ['R','HR','SO','SB']
+
+    X_features = df[features_to_test].fillna(0)
+    y_target = df['W']
+
+    selector = SelectKBest(score_func=mutual_info_regression, k=2)
+
+    selector.fit(X_features, y_target)
+
+    winning_features = selector.get_support()
+    best_features = X_features.columns[winning_features].tolist()
+
+    print(best_features)
+
+    # Spliting Data
+
+    X = df[best_features]
+    y = df['H']
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    print(f"Training Data Size: {X_train.shape}")
+    print(f"Testing Data Size: {X_test.shape}\n")
+
+    # Training Model
+
+    model=LinearRegression()
+    model.fit(X_train,y_train)
+
+    prediction = model.predict(X_test)
+    print(prediction)
+
+    # Handling Missing Values
+    print("Artificially creating missing data for the issues")
+
+    df.loc[0:24, 'H'] = np.nan
+
+    imputer = SimpleImputer(strategy='median')
+
+    df['H'] = imputer.fit_transform(df[['H']]).ravel()
+
+    print(f"Imputation complete. 'H' now has {df['H'].isnull().sum()} null values.\n")
+
+    print("Evaluating the skewness of the Runs (R) distribution...")
+
+    df['LogRuns'] = np.log1p(df['R'])
+
+    print(f"Log Transformation applied. New skewness: {df['LogRuns'].skew():.2f} (closer to 0 is perfectly balanced).\n")
+
+
+if __name__ == "__main__":
+    main()
